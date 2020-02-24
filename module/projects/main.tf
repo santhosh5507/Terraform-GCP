79b3740ff699e7ee204971a24b42dc07285e83a5
santhosh5507 <56131302+santhosh5507@users.noreply.github.com>
2020-02-24 22:30:36 +0530

    Update main.tf

--- module/projects/main.tf
+++ module/projects/main.tf
@@ -3,12 +3,12 @@ resource "google_project" "svpc-host-project" {
   name       = var.host-project-name #"sharedvpc-host-project"
   project_id = var.host-project-id #"sharedvpc-host-project12345"
   # # org_id     = var.org_id -organization id - this is optional. While creating a project either org_id or folder_id only 1 attribute should be given
-  folder_id  = var.folder1-id #google_folder.gcp_folder1.name # this project will be created under gcp_folder1
-  billing_account = "1234-5678-9012" # billing account id - this is optional
+  # folder_id  = var.folder1-id #google_folder.gcp_folder1.name # this project will be created under gcp_folder1
+  # billing_account = "1234-5678-9012" # billing account id - this is optional
   auto_create_network = false # disabling the default network creation
   skip_delete = false # This is optional. If true, the Terraform resource can be deleted without deleting the Project via the Google API.
 }
-
+/*
 # Under Folder1 - creating a service project1 which uses shared vpc network from the host project
 resource "google_project" "service-project1" {
   name       = var.service-project1-name # "image-repo-project"
@@ -63,4 +63,4 @@ resource "google_project" "monitoring-project" {
   auto_create_network = false # disabling the default network creation
   skip_delete = false # This is optional. If true, the Terraform resource can be deleted without deleting the Project via the Google API.
 }
-
+*/
