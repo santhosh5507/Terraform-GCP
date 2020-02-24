b32cfeb6887c2685ad15a800471ce326e151fba8
santhosh5507 <56131302+santhosh5507@users.noreply.github.com>
2020-02-24 22:35:49 +0530

    Update outputs.tf

--- module/projects/outputs.tf
+++ module/projects/outputs.tf
@@ -1,7 +1,7 @@
 output "host-project-id" {
     value = google_project.svpc-host-project.project_id
 }
-
+/*
 output "service-project1-id" {
     value = google_project.service-project1.project_id
 }
@@ -9,3 +9,4 @@ output "service-project1-id" {
 output "service-project2-id" {
     value = google_project.service-project2.project_id
 }
+*/
