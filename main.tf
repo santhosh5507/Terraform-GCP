565a17e7e5001bcb737d6d888c9813375f698657
santhosh5507 <56131302+santhosh5507@users.noreply.github.com>
2020-02-24 22:16:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  # credentials = "./login.json"
+  # region      = "us-east4"
+}
+
+module "project" {
+  source = "./modules/projects"
+
+  folder1-id = "shared-services12345" # provide folder1 id
+
+  host-project-name = "regen-svpc-host"
+  host-project-id = "regen-svpc-host12345"
+
+  service-project1-name = "image-project"
+  service-project1-id = "image-project12345"
+
+  service-project2-name = "mgmt-project"
+  service-project2-id = "mgmt-project12345"
+
+  folder2-id = "management12345" # provide folder2 id
+  
+  log-project-name = "log-sink-project"
+  log-project-id = "log-sink-project12345"
+
+  billing-project-name = "billing-export-project"
+  billing-project-id = "billing-export-project12345"
+
+  monitoring-project-name = "std-monitoring-project"
+  monitoring-project-id = "std-monitoring-project12345"
+  
+}
