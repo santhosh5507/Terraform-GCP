6d6fc662f0c49d8cea4de6ae38cb9f7320d689f7
santhosh5507 <56131302+santhosh5507@users.noreply.github.com>
2020-02-24 22:21:20 +0530

    Create variables.tf

---
+++ module/projects/variables.tf
@@ -0,0 +1,69 @@
+variable "folder1-id" {
+  type        = string
+  description = "id of the folder"
+}
+
+variable "folder2-id" {
+  type        = string
+  description = "id of the folder"
+}
+
+variable "host-project-name" {
+  type        = string
+  description = "name of the host project"
+}
+
+  variable "host-project-id" {
+  type        = string
+  description = "id of the host project"
+}
+
+  variable "service-project1-name" {
+  type        = string
+  description = "name of the service project1"
+}
+
+  variable "service-project1-id" {
+  type        = string
+  description = "id of the service project1"
+}
+
+  variable "service-project2-name" {
+  type        = string
+  description = "name of the service project 2"
+}
+
+  variable "service-project2-id" {
+  type        = string
+  description = "id of the service project 2"
+}
+
+variable "log-project-name" {
+  type        = string
+  description = "name of the log-sink project"
+}
+
+  variable "log-project-id" {
+  type        = string
+  description = "id of the log-sink project"
+}
+
+variable "billing-project-name" {
+  type        = string
+  description = "name of the billing-export project"
+}
+
+  variable "billing-project-id" {
+  type        = string
+  description = "id of the billing-export project"
+}
+
+variable "monitoring-project-name" {
+  type        = string
+  description = "name of the monitoring project"
+}
+
+  variable "monitoring-project-id" {
+  type        = string
+  description = "id of the monitoring project"
+}
